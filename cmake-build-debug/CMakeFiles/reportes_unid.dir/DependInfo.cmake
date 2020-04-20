3440135b1d24c1eb1c1fd2bf5f8a169eadb16ebf
Andres Aguilar <andresaguilar@MacBook-Pro-de-Andres.local>
2020-04-19 20:37:18 -0500

    primera version

---
+++ cmake-build-debug/CMakeFiles/reportes_unid.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/andresaguilar/CLionProjects/reportes-unid/funciones.cpp" "/Users/andresaguilar/CLionProjects/reportes-unid/cmake-build-debug/CMakeFiles/reportes_unid.dir/funciones.cpp.o"
+  "/Users/andresaguilar/CLionProjects/reportes-unid/main.cpp" "/Users/andresaguilar/CLionProjects/reportes-unid/cmake-build-debug/CMakeFiles/reportes_unid.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/reportes_unid.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/reportes_unid.dir/funciones.cpp.o"
+  "CMakeFiles/reportes_unid.dir/main.cpp.o"
+  "reportes_unid"
+  "reportes_unid.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/reportes_unid.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
